b4c043cc2752877874913b6309036164cc10a844
gunderbus <cole.gundermann@gmail.com>
2026-03-26 23:28:09 -0400

    kanye please drop bully

--- src/playerbase/integrations/ghapi.py
+++ src/playerbase/integrations/ghapi.py
@@ -6,10 +6,12 @@ def get_repo(owner: str, repo: str, token: str) -> dict:
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {token}",
     }
-
-    response = requests.get(url, headers=headers, timeout=10)
-    response.raise_for_status()
-    return response.json()
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except:
+        print("Unable to access github to get the repo, check code and token.")
 
 def get_commit(repo: dict, commit_id: str) -> dict:
-     # work here
+     # work here    
