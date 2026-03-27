d1a41a23844d3beed2dd9a595e46a1b669de86e7
gunderbus <cole.gundermann@gmail.com>
2026-03-26 23:48:31 -0400

    im gonna die

--- src/playerbase/integrations/ghapi.py
+++ src/playerbase/integrations/ghapi.py
@@ -2,10 +2,12 @@ import requests
 
 def get_repo(owner: str, repo: str, token: str) -> dict:
     url = f"https://api.github.com/repos/{owner}/{repo}"
+
     headers = {
         "Accept": "application/vnd.github+json",
         "Authorization": f"Bearer {token}",
     }
+
     try:
         response = requests.get(url, headers=headers, timeout=10)
         response.raise_for_status()
@@ -13,5 +15,16 @@ def get_repo(owner: str, repo: str, token: str) -> dict:
     except:
         print("Unable to access github to get the repo, check code and token.")
 
-def get_commit(repo: dict, commit_id: str) -> dict:
+def get_commit(owner: str, repo: str, commit_sha: str, token: str) -> dict:
      # work here    
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits/{commit_sha}
+
+    headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {token}",
+    }
+    
+    parker = requests.get(url, headers=headers, timeout=10)
+    parker.raise_for_status()
+    return parker.json()
