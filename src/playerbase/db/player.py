2264226fab3deae1c0d975f46f88d3bbee5a89fe
gunderbus <cole.gundermann@gmail.com>
2026-03-28 22:47:55 -0400

    kanye

--- src/playerbase/db/player.py
+++ src/playerbase/db/player.py
@@ -11,6 +11,7 @@ class Player:
     _id: Optional[int] = None
     dispname: Optional[str] = field(default=None)
     score: int = 0
+    rank: str = "Rookie"
 
     def __post_init__(self) -> None:
         if self.dispname is None:
@@ -20,4 +21,21 @@ class Player:
         self.score += points
 
     def get_commit(self, repo: str, commit_sha: str):
-        return ghapi.get_commit(self.username, repo, commit_sha, self.token)
+        try:
+            return ghapi.get_commit(self.username, repo, commit_sha, self.token)
+        except:
+            print("interesting")
+            return 0
+    
+    def get_rank(self) -> str:
+        try:
+            return self.rank
+        except:
+            print("uhhhhhhhhhhhhhhh")
+            return "vro ts aint a rank"
+    
+    def set_rank(self, ranking: str) -> None:
+        if(ranking != " " or ranking != "" or ranking != None):
+            self.rank = ranking
+        else:
+            print("Brotien shake YOU NEED TO ENTER TS")
